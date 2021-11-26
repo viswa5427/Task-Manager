64d2ebd66a94a60812810b74513191114ed14abf
viswa5427 <kvn8501979409@gmail.com>
2021-11-26 07:17:31 +0530

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Task-Manager
+# TaskManager
+<img src="Images/TaskManager.png" width=100% height=70%>
+## Usage
+
+### Install dependencies
+
+```
+npm install
+```
+
+### Run Angular server (http://localhost:4200)
+
+```
+ng serve
+```
+
+### Run the JSON server (http://localhost:5000)
+
+```
+npm run server
+```
+
+### To build for production
+
+```
+ng build
+```
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Project Discussion',
+    day: 'Nov 25th at 8:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Juspay Exam',
+    day: 'Nov 25th at 9:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Coding Session',
+    day: 'Nov 25th at 10:30pm',
+    reminder: false,
+  },
+];
